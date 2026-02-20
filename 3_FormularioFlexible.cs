ded0ce1ecd2da274e58ffe54c374a0a468251c47
wirkin106-dev <wirkin106@gmail.com>
2026-02-19 22:51:45 -0400

    Refactor comments in CrearPerfil function

--- 3_FormularioFlexible.cs
+++ 3_FormularioFlexible.cs
@@ -4,11 +4,10 @@ namespace FormularioFlexible
 {
     class Program
     {
-        // 1. Función CrearPerfil
-        // hobby es opcional con default "Dormir"
+        
         static void CrearPerfil(string hobby = "Dormir")
         {
-            // ✅ pide el nombre al usuario
+            // pide el nombre al usuario
             Console.WriteLine("Ingresa tu nombre:");
             string nombre = Console.ReadLine();
 
@@ -20,11 +19,11 @@ namespace FormularioFlexible
 
         static void Main(string[] args)
         {
-            // ✅ 4 y 5. Primera llamada CON hobby
+            // Primera llamada CON hobby
             Console.WriteLine("=== PRIMER PERFIL ===");
             CrearPerfil("Programar");
 
-            // ✅ 4 y 5. Segunda llamada SIN hobby
+            // Segunda llamada SIN hobby
             Console.WriteLine("=== SEGUNDO PERFIL ===");
             CrearPerfil();
         }
